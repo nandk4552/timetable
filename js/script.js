c3a4c4c67ae94c7cf147e94fac421dab037b7d79
Nand Kishore <nandk4552@gmail.com>
2022-05-15 10:14:17 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,21 +1,12 @@
 window.onload = function () {
     document.getElementById('preloader').style.display = 'none';
 }
-let menutoggle = document.querySelector('.toggle');
-menutoggle.onclick = function () {
-    menutoggle.classList.toggle('active');
-}
+
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
-})
-var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
-    trigger: 'focus'
 })
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-$(document).ready(function () {
-    $("#myTable").DataTable();
-});

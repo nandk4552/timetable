155077826b8232fe86ab675cbe003ee408d0f64d
Nand Kishore <nandk4552@gmail.com>
2022-05-16 09:49:57 +0530

    notification section added

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 window.onload = function () {
     document.getElementById('preloader').style.display = 'none';
 }
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-})
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+    toast.show()
+  })
+}
